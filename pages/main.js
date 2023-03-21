5bae53cc37211a9c81b11ef9e2fece91b909648d
N3zZa <nazar.developer007@gmail.com>
2023-03-21 23:44:27 +0300

    done

--- pages/main.js
+++ pages/main.js
@@ -33,7 +33,7 @@ xhr.onreadystatechange = function () {
 
 function showMovies(data) {
     const moviesEl = document.querySelector(".movies")
-    data.results.forEach(movie => {
+   /*  data.results.forEach(movie => {
         const movieEl = document.createElement('div')
         movieEl.className = "nv-el";
         movieEl.setAttribute("nv-el", "");
@@ -61,6 +61,40 @@ function showMovies(data) {
         movieEl.appendChild(year);
 
         moviesEl.appendChild(movieEl);
-    })
+    }) */
+    let i = 0;
+    while (i < data.results.length) {
+      const movieEl = document.createElement("div");
+      movieEl.className = "nv-el";
+      movieEl.setAttribute("nv-el", "");
+      const img = document.createElement("img");
+      img.setAttribute("src", data.results[i].info.poster);
+      img.setAttribute("alt", "img");
+
+      const filmTitle = document.createElement("div");
+      filmTitle.className = "film-title";
+
+      const h2 = document.createElement("h2");
+      h2.appendChild(document.createTextNode(data.results[i].info.rus));
+
+      const p = document.createElement("p");
+      p.appendChild(
+        document.createTextNode(data.results[i].info.rating.rating_imdb)
+      );
+
+      filmTitle.appendChild(h2);
+      filmTitle.appendChild(p);
+
+      const year = document.createElement("p");
+      year.appendChild(document.createTextNode(data.results[i].info.year));
+
+      movieEl.appendChild(img);
+      movieEl.appendChild(filmTitle);
+      movieEl.appendChild(year);
+
+      moviesEl.appendChild(movieEl);
+      i++
+    }
+
     moviesEl.firstElementChild.setAttribute("nv-el-current", "true");
 }
