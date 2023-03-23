65b52d84a2a572628b3510ae7531caa5b750d01d
N3zZa <nazar.developer007@gmail.com>
2023-03-23 23:07:16 +0300

    efe

--- index.js
+++ index.js
@@ -84,11 +84,11 @@ const path = require('path');
 
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/index.html"));
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 
 app.get('/public/Anime', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/views/Anime.html'));
+    res.sendFile(path.join(__dirname + '/views/Anime.html'));
 });
 
 app.listen(8080);
