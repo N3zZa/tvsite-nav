f95d217b9fb87ff855568903096413acefe1c8a3
N3zZa <nazar.developer007@gmail.com>
2023-03-23 23:01:34 +0300

    wfaf

--- index.js
+++ index.js
@@ -82,6 +82,11 @@ app.use(express.static('public'));
 
 const path = require('path');
 
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/public/index.html"));
+});
+
 app.get('/public/Anime', (req, res) => {
     res.sendFile(path.join(__dirname + '/views/Anime.html'));
 });
